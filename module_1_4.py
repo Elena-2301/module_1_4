38131c2fd6964f0056032dabd30e3805a37b5160
Elena-2301 <elena.pehnova@gmail.com>
2024-10-11 10:33:07 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,20 @@
+#name = input('Введите Ваше имя: ')
+##print('Здравствуйте,', name)
+#current_year = 2024
+#date_of_birth = int(input('В каком году Вы родились? '))
+#age = current_year - date_of_birth
+#print(' Здравствуйте,', name)
+#print('В этом году Вам ', age, 'лет')
+
+#print('Привет, я строка'.upper())
+#print('Привет, я строка'.lower())
+#print('Привет, я строка'.replace("Привет","Пока"))
+#print('Привет, я строка'.replace(" ","_"))
+
+# Практическое работа к уроку
+my_string = input('Напишите текст: ')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace((' '),('_')))
+print(my_string[:1])
+print(my_string[-1])
